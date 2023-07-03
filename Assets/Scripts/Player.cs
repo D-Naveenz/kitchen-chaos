d7c474008127257829d649f43e3db3e039b9b9de
Naveen Dharmathunga <dasheenaveen@outlook.com>
2023-07-03 19:12:08 +0530

    Initiate counters and interaction system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,35 +5,33 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
-    [SerializeField] private float rotateSpeed = 10f;
     [SerializeField] private GameInput gameInput;
-    private bool isWalking;
     
-    public bool IsWalking
-    { 
-        get
-        {
-            return isWalking;
-        }
-    }
+    public bool IsWalking { get; private set; }
 
     // Update is called once per frame
     private void Update()
     {
         Vector2 inputVector = gameInput.GetMovemRentVectorNormalized();
+        Vector3 moveDirection = new(inputVector.x, 0f, inputVector.y);  // 2D plane of the inputVector is in horizontaly. So we need to convert it to a vertical plane using a 3D vector
 
-        // 2D plane of the inputVector is in horizontaly. So we need to convert it to a vertical plane using a 3D vector
-        Vector3 moveDirection = new(inputVector.x, 0f, inputVector.y);
+        HandleMovement(moveDirection);
+        HandleInteractions();
+    }
 
+    private void HandleMovement(Vector3 moveDirection)
+    {
+        float rotateSpeed = moveSpeed / 2 * 3;
         float playerRadius = 0.7f;
         float playerHeight = 2f;
         float moveDistance = moveSpeed * Time.deltaTime;
+
         // checking whether the player can move in the direction of the moveDirection
         if (!Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirection, moveDistance))
         {
-            transform.position += moveDirection * moveDistance; 
+            transform.position += moveDirection * moveDistance;
             // setting the animation
-            isWalking = moveDirection != Vector3.zero;
+            IsWalking = moveDirection != Vector3.zero;
         }
         else
         {
@@ -46,23 +44,38 @@ public class Player : MonoBehaviour
                 moveDirection = moveToX;
                 transform.position += moveDirection * moveDistance;
                 // setting the animation
-                isWalking = moveDirection != Vector3.zero;
+                IsWalking = moveDirection != Vector3.zero;
             }
             else if (!Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveToZ, moveDistance))
             {
                 moveDirection = moveToZ;
                 transform.position += moveDirection * moveDistance;
                 // setting the animation
-                isWalking = moveDirection != Vector3.zero;
+                IsWalking = moveDirection != Vector3.zero;
             }
             else
             {
                 // player cann't move to any direction
-                //isWalking = false;
+                IsWalking = false;
             }
         }
 
         // rotating the player according to the movement direction
         transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
+    }
+
+    private void HandleInteractions()
+    {
+        float interactDistance = 2f;
+        Vector3 InteractDirection = transform.forward;
+
+        // checking whether the player can interact with the object in front of him
+        if (Physics.Raycast(transform.position, InteractDirection, out RaycastHit hit, interactDistance))
+        {
+            if (hit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                clearCounter.Interact();
+            }
+        }
     }
 }
